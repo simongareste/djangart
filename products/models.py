95b819b915f19992218bd7f494aec1f4fa1ff4c5
Simon Gareste <simon.gareste@arturia.com>
2016-02-11 17:33:28 +0100

    Arturia Django project - products step 1

---
+++ products/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+  product_sku = models.CharField(max_length=50)
+  product_type = models.CharField(max_length=2)
+  desc = models.CharField(max_length=200)
+  product_generation = models.IntegerField(default=0)
+  is_pack = models.IntegerField(default=0)
+  bundle = models.IntegerField(default=0)
+  collection = models.IntegerField(default=0)
+
+  def __str__(self):
+    return self.product_sku+str(self.id)
+
+class ProductsBundle(models.Model):
+  protection = models.CharField(max_length=50)
+  bundler_product = models.ForeignKey(Product,related_name='%(class)s_bundler_product_id',on_delete=models.CASCADE)
+  product = models.ForeignKey(Product,related_name='%(class)s_product_id',on_delete=models.CASCADE)
+  comment = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.bundler_product+"-"+self.product
+
